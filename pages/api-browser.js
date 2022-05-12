015ede03bc5e0bbd50062093eaef39f4f017dc86
Ian <ian@designbureau.agency>
2022-05-12 22:07:56 +0100

    added filter to api browser

--- pages/api-browser.js
+++ pages/api-browser.js
@@ -8,12 +8,18 @@ export default function ApiBrowser() {
   const [isLoading, setLoading] = useState(false);
   const [systemData, setSystemData] = useState(null);
 
+  const [name, setName] = useState('');
+  const [foundSystems, setFoundSystems] = useState(null);
+ 
+
   useEffect(() => {
     // setLoading(true);
     fetch("api/systems/all")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
+        setFoundSystems(data);
+
         // setLoading(false);
       });
   }, []);
@@ -41,29 +47,87 @@ export default function ApiBrowser() {
         });
   };
 
+
+
+  const filter = (e) => {
+    const keyword = e.target.value;
+
+    if (keyword !== '') {
+      const results = data.systems.filter((system) => {
+        return system.toLowerCase().includes(keyword.toLowerCase());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+      setFoundSystems(results);
+    } else {
+      setFoundSystems(data.systems);
+      // If the text field is empty, show all users
+    }
+
+    setName(keyword);
+  };
+
+
   return (
     <div className="grid">
-      <div>
-      <h3>Endpoints:</h3>
-      <ul>
+      <div class="api-nav">
+      <h3>Endpoints</h3>
+      <ul className="pb-0 api-menu">
         <li><Link href="/api/systems/all">/api/systems/all</Link></li>
         <li><Link href={`/api/systems/${systemData? systemData.name : "24 Boo"}`}>{`${systemData? "/api/systems/"+ encodeURIComponent(systemData.name) : "/api/systems/{system}" }`}</Link></li>
       </ul>
-      <ul>
-        {data.systems.map((system) => {
-          return (
-            <li key={system}>
-              <button
-                onClick={() => {
-                  clickHandler(system);
-                }}
-              >
-                {system}
-              </button>
-            </li>
-          );
-        })}
+      <ul className="navigation active relative mt-0">
+      <li className="hasSubMenu">
+          <h3 className="text-white">Systems</h3>
+
+          <input
+            type="search"
+            value={name}
+            onChange={filter}
+            className="input filter"
+            placeholder="Filter"
+          />
+
+          <ul className="subMenu">
+            {
+            foundSystems && foundSystems.length > 0 ? (
+            foundSystems.map((system) => {
+              return (
+                <li key={system}>
+                  <button
+                    onClick={() => {
+                      clickHandler(system);
+                    }}
+                  >
+                    {system}
+                  </button>
+                </li>
+              );
+            })) : 
+            
+            data && data.systems.map((system) => {
+                return (
+                  <li key={system}>
+                    <button
+                      onClick={() => {
+                        clickHandler(system);
+                      }}
+                    >
+                      {system}
+                    </button>
+                  </li>
+                );
+              })
+            
+            
+            }
+          </ul>
+        </li>
+        
       </ul>
+
+
+
+
       </div>
       {systemData && (<pre>
         { isLoading ? <p>Loading...</p> : JSON.stringify(systemData, null, 2)}
@@ -71,3 +135,5 @@ export default function ApiBrowser() {
     </div>
   );
 }
+
+
