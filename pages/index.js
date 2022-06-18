c3e7aa55d77894e1c48cf7a5c3b7ccd21663298d
Ian Jamieson <ian@designbureau.agency>
2022-06-18 23:11:27 +0100

    add initial focus

--- pages/index.js
+++ pages/index.js
@@ -5,47 +5,53 @@ import { useRef, useState, useMemo, forwardRef, useEffect } from "react";
 import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
 import SkyBox from "../components/SkyBox";
 import CreateSystem from "../components/CreateSystem";
-import Controls from '../components/CameraControls';
+import Controls from "../components/CameraControls";
 import { EnvContext, Constants } from "../components/EnvContext";
-import { Perf } from 'r3f-perf';
+import { Perf } from "r3f-perf";
 import { useKeyState } from "use-key-state";
 
 export default function Home() {
   const [systemData, setSystemData] = useState(null);
   const [cursor, setCursor] = useState("default");
-  const [focus, setFocus] = useState();
   const [clicked, setClicked] = useState(false);
-
   const refs = useRef(new Array());
+  const [focus, setFocus] = useState(null);
 
   const [viewState, setViewState] = useState({
     clicked: false,
-    focus: null
+    focus: null,
   });
-
 
   const [refsArray, setRefsArray] = useState(new Array());
 
+  if (Array.isArray(refs.current)) {
+    if (refs.current[0]) {
+      console.log(focus);
+      if (focus === null || focus.current === null) {
+        setFocus(refs.current[0].star);
+      }
+    }
+  }
 
-
-  // const refs = {};
-  console.log("refs", refs);
-
-  // console.log("index", {refs});
-
-
+  // console.log({refs});
 
   return (
     <>
       <Nav setSystemData={setSystemData} refs={refs} />
-      {systemData && (<SystemNav refs={refs} setFocus={setFocus} setClicked={setClicked} />)}
-      <div id="canvas-container" style={{cursor:cursor}}>
-          <Canvas
-            dpr={[1, 2]}
-          >
-            <EnvContext.Provider value={Constants}>
-              {useMemo(() => <SkyBox />,[])}
-              {systemData && (<CreateSystem
+      {systemData && (
+        <SystemNav refs={refs} setFocus={setFocus} setClicked={setClicked} />
+      )}
+      <div id="canvas-container" style={{ cursor: cursor }}>
+        <Canvas dpr={[1, 2]}>
+          <EnvContext.Provider value={Constants}>
+            {useMemo(
+              () => (
+                <SkyBox />
+              ),
+              []
+            )}
+            {systemData && (
+              <CreateSystem
                 systemData={systemData}
                 setFocus={setFocus}
                 setClicked={setClicked}
@@ -53,13 +59,19 @@ export default function Home() {
                 viewState={viewState}
                 setRefsArray={setRefsArray}
                 refs={refs}
-              />)}
-            </EnvContext.Provider>
-            <Controls focus={focus} setFocus={setFocus} clicked={clicked} setClicked={setClicked} />
-            <Perf />
-          </Canvas>
+              />
+            )}
+          </EnvContext.Provider>
+          <Controls
+            focus={focus}
+            setFocus={setFocus}
+            clicked={clicked}
+            setClicked={setClicked}
+          />
+          <Perf />
+        </Canvas>
       </div>
-      <div className="controls-info">Controls: Move:w,a,s,d | Rotation:up,down,left,right | Sensitivity:+,- | Reset target: enter</div>
+      {/* <div className="controls-info">Controls: Move:w,a,s,d | Rotation:up,down,left,right | Sensitivity:+,- | Reset target: enter</div> */}
     </>
   );
 }
