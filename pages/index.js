cbc1f82aaa5a8ebde68a03b258df22e05015b032
Ian Jamieson <ian@designbureau.agency>
2022-06-25 18:31:07 +0100

    hide performance metrics

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
             initialTarget={initialTarget}
             follow={follow}
           />
-          <Perf />
+          {/* <Perf /> */}
         </Canvas>
       </div>
       <div className="controls">
